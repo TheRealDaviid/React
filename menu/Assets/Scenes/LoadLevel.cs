f53e975bea06a5f45cc64434758d59abc9eb983f
TheRealDaviid <43785752+TheRealDaviid@users.noreply.github.com>
2019-01-16 23:04:08 +0100

    Multiplayer

---
+++ menu/Assets/Scenes/LoadLevel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevel : MonoBehaviour
+{
+    public string sceneToLoad;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    // Start is called before the first frame update
+    
+    public void playGame()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+
+  
+}
