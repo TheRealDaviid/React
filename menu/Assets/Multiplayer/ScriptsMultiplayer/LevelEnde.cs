af83a1f8d19388ff1520f44951a915a6c780c112
TheRealDaviid <43785752+TheRealDaviid@users.noreply.github.com>
2019-01-17 13:08:53 +0100

    PART 2 FINISHED

---
+++ menu/Assets/Multiplayer/ScriptsMultiplayer/LevelEnde.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelEnde : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            SceneManager.LoadScene("Menu");
+        }
+        
+
+    }
+}
