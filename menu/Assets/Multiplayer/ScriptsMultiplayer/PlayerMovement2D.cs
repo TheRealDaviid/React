e635f1f97c33aebf768d3266b6b214e5d9c72856
TheRealDaviid <43785752+TheRealDaviid@users.noreply.github.com>
2019-01-18 08:37:35 +0100

    Fehler ausgebessert

--- menu/Assets/Multiplayer/ScriptsMultiplayer/PlayerMovement2D.cs
+++ menu/Assets/Multiplayer/ScriptsMultiplayer/PlayerMovement2D.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class PlayerMovement2D : MonoBehaviour
 {
-    public CharacterController2D controller;
+   // public CharacterController2D controller;
 
     public KeyCode left;
     public KeyCode right;
     public KeyCode jump;
+
     public Rigidbody2D player;
     float horizontalMove = 0f;
-    public float jumpforce = 0f;
     private bool m_Grounded;
     private float m_JumpForce = 400f;
 
@@ -54,7 +54,7 @@ public class PlayerMovement2D : MonoBehaviour
 
         if (Input.GetKeyDown(jump))
         {
-            player.velocity = new Vector2(player.velocity.x, jumpforce);
+            player.velocity = new Vector2(player.velocity.x, m_JumpForce);
             if (m_Grounded && jump1)
             {
                 // Add a vertical force to the player.
