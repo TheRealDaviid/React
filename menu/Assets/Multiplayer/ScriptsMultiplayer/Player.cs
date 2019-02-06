060af3a7e88a0bb04d18ba0cff4387a009fb5abc
TheRealDaviid <43785752+TheRealDaviid@users.noreply.github.com>
2019-02-06 15:16:17 +0100

    versucht fehler zu behebn

--- menu/Assets/Multiplayer/ScriptsMultiplayer/Player.cs
+++ menu/Assets/Multiplayer/ScriptsMultiplayer/Player.cs
@@ -13,6 +13,8 @@ public class Player : MonoBehaviour
     public GameObject aufgabe;
     public Text rechnung;
     public GameObject player;
+
+    public int erg = 0;
     void Start()
     {
         scoreCounter = 0;
@@ -25,22 +27,22 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
-        
-        
-        if(collision.tag == "Checkpoint" || collision.tag == "Enemy")
+
+
+        if (collision.tag == "Checkpoint" || collision.tag == "Enemy")
         {
             player.SetActive(false);
             aufgabe.SetActive(true);
             int random1 = Random.Range(1, 90);
             int random2 = Random.Range(1, 90);
-            int erg = 0;
-            int randomoperator = Random.Range(1, 2);
             
+            int randomoperator = Random.Range(1, 2);
+
             if (randomoperator == 1)
             {
                 erg = random1 + random2;
@@ -64,26 +66,30 @@ public class Player : MonoBehaviour
                 PlayerPrefs.SetInt("Highscore", scoreCounter);
                 highScore.text = "Highscore: " + PlayerPrefs.GetInt("Highscore").ToString();
             }
-            
-            if (erg == int.Parse(ergebnis.text))
-            {
-                scoreCounter++;
-                scoreText.text = "Score: " + scoreCounter.ToString(); //geht auch ohne ToString() ist aber sicherer
-            }
-            else
-            {
-                scoreCounter--;
-                scoreText.text = "Score: " + scoreCounter.ToString(); //geht auch ohne ToString() ist aber sicherer
-            }
 
 
-            
+
+
+
         }
-        
+
     }
     public void Close()
     {
+
+        if (erg == int.Parse(ergebnis.text))
+        {
+            scoreCounter++;
+            scoreText.text = "Score: " + scoreCounter.ToString(); //geht auch ohne ToString() ist aber sicherer
+        }
+        else
+        {
+            scoreCounter--;
+            scoreText.text = "Score: " + scoreCounter.ToString(); //geht auch ohne ToString() ist aber sicherer
+        }
         aufgabe.SetActive(false);
         player.SetActive(true);
     }
 }
+
+   
